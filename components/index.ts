bfd459b1fb44968d4e9e3fe696d7f9745a9ad004
nada-hesham <85525572+NA-MO@users.noreply.github.com>
2024-07-22 17:07:54 +0300

    commit all

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+import { SearchManuFacturerProps } from './../types/index';
+import CustomButton  from './CustomButton';
+import Hero  from './Hero';
+import Navbar  from './Navbar';
+import Footer  from './Footer';
+import CustomFilter  from './CustomFilter';
+import SearchBar  from './SearchBar';
+import SearchManufacturer  from './SearchManufacturer';
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+}
